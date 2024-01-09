e273571b4fbc4c7d630a7b19b4cb173f9e4efd97
Huriel Ferreira Lopes <huriel.lopes@compasso.com.br>
2024-01-09 11:30:47 -0300

    fix(header): icon size on mobile devices

--- src/components/commons/Header/HeaderIcon.tsx
+++ src/components/commons/Header/HeaderIcon.tsx
@@ -1,11 +1,20 @@
 import { ComponentProps } from 'react';
 import { Leafy, LeafySm } from '@/assets/icons';
 import Link from 'next/link';
+import { cn } from '@/lib/style';
 
 type HeaderIconProps = ComponentProps<'svg'> & {
   isSearching: boolean;
 };
 
-export const HeaderIcon = ({ isSearching, ...props }: HeaderIconProps) => {
-  return <Link href="/">{isSearching ? <LeafySm {...props} /> : <Leafy {...props} />}</Link>;
+export const HeaderIcon = ({ isSearching, className, ...props }: HeaderIconProps) => {
+  return (
+    <Link href="/">
+      {isSearching ? (
+        <LeafySm {...props} className={cn(className, 'w-[32px]')} />
+      ) : (
+        <Leafy {...props} className={cn(className, 'w-[95px]')} />
+      )}
+    </Link>
+  );
 };
