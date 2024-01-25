4040350d9ae5de5389a0ddcda6903622f8dbd85b
Huriel Ferreira Lopes <huriel.lopes@compasso.com.br>
2024-01-25 14:17:22 -0300

    feat(lf7): use shadcn sheet

--- src/components/commons/Menu/index.tsx
+++ src/components/commons/Menu/index.tsx
@@ -1,28 +1,23 @@
 import { Menu as MenuIcon } from 'lucide-react';
-import * as Dialog from '@radix-ui/react-dialog';
 import { MenuOptionsContainer } from './MenuOptionsContainer';
-
-import { TMenuOption } from './MenuOption';
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTrigger } from '@/components/ui/sheet';
 import { dict } from '@/content/dictionary';
 
 export const Menu = () => {
   return (
-    <Dialog.Root>
-      <Dialog.Trigger className="group">
+    <Sheet>
+      <SheetTrigger>
         <MenuIcon className="w-7 h-7 stroke-lf-gray-600 shrink-0" />
-      </Dialog.Trigger>
-      <Dialog.Overlay className="fixed inset-0 bg-black/40 z-[60]" />
-
-      <Dialog.Content className="fixed top-0 right-0 w-5/6 h-full bg-lf-gray-100 p-4 group-data-[state=open]:bg-blue transition-all animate-expand-menu z-[61]">
-        <div className="flex flex-col justify-between h-full">
-          <div className="space-y-6">
-            <Dialog.Close>
-              <MenuIcon className="w-7 h-7 stroke-lf-gray-600 shrink-0" />
-            </Dialog.Close>
-            <MenuOptionsContainer items={dict('menu.items') as TMenuOption[]} />
-          </div>
-        </div>
-      </Dialog.Content>
-    </Dialog.Root>
+      </SheetTrigger>
+      <SheetContent className="py-2 px-4">
+        <SheetHeader className="mb-6">
+          <SheetClose>
+            <MenuIcon className="w-7 h-7 stroke-lf-gray-600 shrink-0" />
+            <span className="sr-only">{dict('general.close')}</span>
+          </SheetClose>
+        </SheetHeader>
+        <MenuOptionsContainer items={dict('menu.items')} />
+      </SheetContent>
+    </Sheet>
   );
 };
